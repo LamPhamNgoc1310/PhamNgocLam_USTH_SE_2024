21db98cb0bbb2f77bcabc88b690ba6be34d3d5af
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-05-18 09:02:12 +0700

    ProgStudentMan finished and On-going test

--- assignments/src/a2_22bi13240/studentman/Test.java
+++ assignments/src/a2_22bi13240/studentman/Test.java
@@ -1,15 +1,33 @@
 package a2_22bi13240.studentman;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Test {
 	public static void main(String args[]) {
-		Student lam1 = new Student(123, "          lam1          ", "0923543588", "hanoi");
-		UndergradStudent lam2 = new UndergradStudent(123456, "lam2", "0923543589", "hanoi1");
-		PostgradStudent lam3 = new PostgradStudent(123456789, "lam3", "0923543580", "hanoi2", 3.9f);
-
-		lam2.setAddress("HCM");
-		System.out.println(lam1.toString());
-		System.out.println(lam2.toString());
-		System.out.println(lam3.toString());
-
+		ProgStudentMan studentMan = new ProgStudentMan();
+		// Student object data
+		Object[] studentData = new Object[]{123, "lam1", "0923543588", "hanoi"};		
+		Student student = studentMan.createStudent(studentData);
+		
+		
+		// UndergradStudent object data
+		Object[] undergradStudentData = new Object[]{123456, "lam2", "0923543589", "hanoi1"};
+		UndergradStudent underGradStudent = (UndergradStudent) studentMan.createStudent(undergradStudentData);
+		
+		// PostgradStudent object data
+		Object[] postgradStudentData = new Object[]{123456789, "lam3", "0923543580", "hanoi2", 3.9f};
+		PostgradStudent postGradStudent = (PostgradStudent) studentMan.createPostgradStudent(postgradStudentData);
+		
+		Student[] students = {student, underGradStudent, postGradStudent};
+		for(int i = 0; i < students.length; i++) {			
+			studentMan.addStudent(students[i]);
+		}
+//		
+//		for (int i = 0; i < students.length; i++) {
+//			studentMan.addStudent(lam1);
+//		}
+//		System.out.println(studentMan.toString());
+//		System.out.println(students[0]);
+//		
 	}
 }
